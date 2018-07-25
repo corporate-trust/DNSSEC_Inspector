f1daa56494c52864e389ddda5f73d172f182c01d
Arthur Naefe <naefe@corporate-trust.de>
2018-07-25 10:14:55 +0200

    Init

---
+++ dnssec.go
@@ -0,0 +1,50 @@
+/*
+Checks:
+	1. Existence of DNSSEC Rr
+		- DNSKEY, DS, NSEC, NSEC3, NSEC3PARAM, RRSIG
+	2. NSEC3 Existence --> Zone Walking
+	3. Key Strength
+*/
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	//"errors"z
+
+	"github.com/miekg/dns"
+)
+
+func main() {
+	checkKey(os.Args[1])
+}
+
+// TODO: Throw error handling
+func dnssecQuery(fqdn string, rrType uint16) dns.Msg {
+	config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
+	c := new(dns.Client)
+	c.Net = "udp"
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(fqdn), rrType)
+	m.Authoritative = true
+	m.RecursionDesired = true
+	r, _, _ := c.Exchange(m, net.JoinHostPort(config.Servers[0], config.Port))
+	if r == nil {
+		r = nil
+	}
+	if r.Rcode != dns.RcodeSuccess {
+		r = nil
+	}
+	return *r
+}
+
+// Checks if the DNSKEY uses accepted (?) algorithms
+func checkKey(fqdn string) {
+	r := dnssecQuery(fqdn, dns.TypeDNSKEY)
+	for i := range r.Answer {
+		fmt.Printf("%v\n", i)
+	}
+}
