317feba4a7ed1774b01bcb7d30afb14a7560d112
Arthur Naefe <naefe@corporate-trust.de>
2018-07-25 14:38:00 +0200

    Recherche zu key strength done

--- dnssec.go
+++ dnssec.go
@@ -9,16 +9,16 @@ Checks:
 package main
 
 import (
+	"fmt"
 	"net"
 	"os"
-
-	//"errors"z
+	"regexp"
 
 	"github.com/miekg/dns"
 )
 
 func main() {
-	dnssecQuery(os.Args[1], dns.TypeANY)
+	checkKey(os.Args[1])
 }
 
 // TODO: Throw error handling
@@ -38,4 +38,43 @@ func dnssecQuery(fqdn string, rrType uint16) dns.Msg {
 		r = nil
 	}
 	return *r
+}
+
+// Checks if the DNSKEY uses accepted (?) algorithms
+// Signature Algorithm:
+// 	1. RSA/SHA-256 (IETF Recom)
+//  2. RSA/SHA-1 (IETF accepted alternative)
+//  -  RSA/MD5 (IETF shouldnt be considered)
+//  Key Length: BSI TR-02102-2
+//  - RSA min 2048 bit bis 2022
+//  - RSA min 3072 bit ab 2023
+//  - DSA min 2000 bit bis 2022
+//	- ECDSA min 250 bis 2022
+
+// Alg:
+// - 0% -- 1 = RSA/MD5
+// - out of context -- 2 = DH
+// - 3 = DSA/SHA-1
+// - out of context -- 4 = EC
+// - 5 = RSA/SHA-1
+// - 6 = DSA/SHA-1/NSEC3
+// - 7 = RSA/SHA-1/NSEC3
+// - 8 = RSA/SHA-256
+// - out of context -- 9
+// - 10 = RSA/SHA-512
+// - out of context 11
+// - 13 = ECDSA P-256 (128bit security) with SHA-256
+// - 14 = ECDSA/Curve P-384 (192bit security) /SHA-384
+// - 15 = Ed25519 (128bit security aim)
+// - 16 = ED448
+
+func checkKey(fqdn string) {
+	r := dnssecQuery(fqdn, dns.TypeDNSKEY)
+	for _, i := range r.Answer {
+		x := regexp.MustCompile("( +?|\t+?)").Split(i.String(), -1)
+		for _, k := range x {
+			fmt.Printf("%v\n", k)
+		}
+	}
+
 }
