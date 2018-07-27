4e79f012e070191c0999b72fbe3b5008013833c5
user <user@debian.pr>
2018-07-27 15:40:05 +0200

    added DSA base64 encoding

--- dnssec/keylength.go
+++ dnssec/keylength.go
@@ -11,11 +11,9 @@ func byteToInt(byteIn []byte, specByte int) (intOut int) {
 	return intOut
 }
 
-func keyLength(keyIn string) (e, n, l int) {
-	// Base64 encoding
+func keyLengthRSA(keyIn string) (e, n, l int) {
 	keyBinary := make([]byte, base64.StdEncoding.DecodedLen(len(keyIn)))
 	base64.StdEncoding.Decode(keyBinary, []byte(keyIn))
-
 	err := keyBinary
 	if err == nil {
 		fmt.Println("Error:", err)
@@ -38,7 +36,22 @@ func keyLength(keyIn string) (e, n, l int) {
 	return e, n, l
 }
 
+func keyLengthDSA(keyIn string) (e, n, l int) {
+	keyBinary := make([]byte, base64.StdEncoding.DecodedLen(len(keyIn)))
+	base64.StdEncoding.Decode(keyBinary, []byte(keyIn))
+	err := keyBinary
+	if err == nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Printf("\nDNS: %s\n", keyBinary)
+	return e, n, l
+}
+
 func main() {
-	keyInput := "AwEAAcPXtQjs85qD8rnBCxGLRcm1Ghc0jWAS8ExiEaKUBK24yp6DpvuqQFevVfFXT3SUcrMw9La9dUHk0ZLFMZTC+irx4+/iaR9UYG6WW7xpWD12l0NotT0Z7GELKk5mCCnWUe72hVolxrvmaMT3J0GcP0FvSqFicuDEjAzYEoGEiYD5"
-	keyLength(keyInput)
+	keyInputRSA := "AwEAAcPXtQjs85qD8rnBCxGLRcm1Ghc0jWAS8ExiEaKUBK24yp6DpvuqQFevVfFXT3SUcrMw9La9dUHk0ZLFMZTC+irx4+/iaR9UYG6WW7xpWD12l0NotT0Z7GELKk5mCCnWUe72hVolxrvmaMT3J0GcP0FvSqFicuDEjAzYEoGEiYD5"
+	keyInputDSA := "BOPdJjdc/ZQWCVA/ONz6LjvugMnB2KKL3F1D2i9GdrpircWRKS2DfRn5KiMM2HQXBHv0ZdkFs/tmjg7rYxrN+bzBNrlwfU5RMjioi67PthD07EHbZjwoZ5sKC2BZ/M596hygfx5JAvbIWBQVF+ztiuCnWCkbGvVXwsmE+odINCur+o+EjA9hF06LqTviUJKqTxisQO5OHM/0ufNenzIbijJPTXbUcF3vW+CMlX+AUPLSag7YnhWaEu7BLCKfg3vJVw9mtaN2W3oWPRdebGUf/QfyVKXoWD6zDLByCZh4wKvpcwgAsel4bO5LVe7s8qstSxqrwzmvaZ5XYOMZFbN7CXtutiswAkb0pkehIYime6IRkDwWDG+14H5yriRuCDK3m7GvwxMo+ggV0k3Po9LD5wWSIi1N"
+	keyLengthRSA(keyInputRSA)
+	keyLengthDSA(keyInputDSA)
 }
