6e11be699b79787e3e8c867b013628c480cfff53
user <user@debian.pr>
2018-07-27 10:16:35 +0200

    keyLength finish

--- dnssec/keylength.go
+++ dnssec/keylength.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"encoding/base64"
-	//"strconv"
 	"fmt"
+	"math/big"
 )
+
+func byteToInt(byteIn []byte, specByte int) (intOut int) {
+	intOut = (int(byteIn[specByte]) << 8) + int(byteIn[specByte+1])
+	return intOut
+}
 
 func keyLength(keyIn string) (e, n, l int) {
 	// Base64 encoding
@@ -18,16 +23,16 @@ func keyLength(keyIn string) (e, n, l int) {
 	}
 
 	if keyBinary[0] == 0 {
-		e := keyBinary[1:3]
-		n := keyBinary[3:]
-		l := len(n) * 8
-		fmt.Printf("e: %s\nn: %s\nl: %d", e, n, l)
+		el := (int(keyBinary[1]) << 8) + int(keyBinary[2])
+		e := new(big.Int).SetBytes(keyBinary[3 : el+3])
+		n := new(big.Int).SetBytes(keyBinary[el+3:])
+		l := len(keyBinary[el+3:]) * 8
+		fmt.Printf("e: %s\nn: %s\nl: %d\n", e, n, l)
 	} else {
-		// requires import "strconv"
-		// e := strconv.ParseInt(keyBinary[1], 2, 64)
-		e := keyBinary[1]
-		n := keyBinary[2:]
-		l := len(n) * 8
+		el := keyBinary[0]
+		e := new(big.Int).SetBytes(keyBinary[1 : el+1])
+		n := new(big.Int).SetBytes(keyBinary[el+1:])
+		l := len(keyBinary[el+1:]) * 8
 		fmt.Printf("e: %s\nn: %s\nl: %d\n", e, n, l)
 	}
 	return e, n, l
@@ -37,35 +42,3 @@ func main() {
 	keyInput := "AwEAAcPXtQjs85qD8rnBCxGLRcm1Ghc0jWAS8ExiEaKUBK24yp6DpvuqQFevVfFXT3SUcrMw9La9dUHk0ZLFMZTC+irx4+/iaR9UYG6WW7xpWD12l0NotT0Z7GELKk5mCCnWUe72hVolxrvmaMT3J0GcP0FvSqFicuDEjAzYEoGEiYD5"
 	keyLength(keyInput)
 }
-
-/*
-func b64Decoder(keyIn string) (keyBinary []byte) {
-	b64String := make([]byte, base64.StdEncoding.DecodedLen(len(keyIn)))
-	base64.StdEncoding.Decode(b64String, []byte(keyIn))
-
-	err := b64String
-	if err == nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	return b64String
-}
-
-
-func keyLength(keyBinary []byte) (e, n, l int) {
-
-	if keyBinary[0] == 0 {
-		e := keyBinary[1:3]
-		n := keyBinary[3:]
-		l := len(n)
-		fmt.Printf("e: %s\nn: %s\nl: %d", e, n, l)
-	} else {
-		e := strconv.ParseInt(keyBinary[1], 2, 64)
-		n := keyBinary[2:]
-		l := len(n)
-		fmt.Printf("e: %s\nn: %s\nl: %d\n", e, n, l)
-	}
-	return e, n, l
-}
-*/
